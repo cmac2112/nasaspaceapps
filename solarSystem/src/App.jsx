4d06de6795aea4ab421257e121fef3ea20f165d1
meriemdhouibi13 <mdhouibi.yestunisia2020@gmail.com>
2024-10-06 04:05:37 -0500

    new147

--- solarSystem/src/App.jsx
+++ solarSystem/src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from './components/landingPage'
 import "./App.css";
+import InfoPage from "./components/InfoPage/InfoPage";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/info" element={<InfoPage/>}/>
       </Routes>
     </Router>
   )
--- solarSystem/src/components/landingPage.tsx
+++ solarSystem/src/components/landingPage.tsx
@@ -2,25 +2,9 @@ import React from 'react'
 import './landingPage.css'
 import Layout from './Layout/Layout'
 const landingPage = () => {
-  return (
-    <Layout>
-      <div className="bg">
-        <p className="test">test</p>
-        <p className="test">test</p>
-        <p className="test">test</p>
-        <p className="test">test</p>
-        <p className="test">test</p>
-        <p className="test">test</p>
-        <p className="test">test</p>
-        <p className="test">test</p>
-        <p className="test">test</p>
-        <p className="test">test</p>
-        <p>test</p>
-        <p>test</p>
-        
-      </div>
-    </Layout>
-  )
+ return (
+  <div>test</div>
+ )
 }
 
 export default landingPage
