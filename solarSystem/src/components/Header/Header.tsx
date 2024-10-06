c4c8e02a9a8442f565f4c20ae353e7b1bf497ede
Caden McArthur <cadenamcarthur@bethelks.edu>
2024-10-05 19:03:07 -0500

    new push

---
+++ solarSystem/src/components/Header/Header.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+    // header component, should hold links to all the pages here
+  return (
+    <div className="header-container">
+        <h1>Solar System</h1>
+        <nav className="navbar">
+            <ul className="list">
+            <li><a href="/">Home</a></li>
+            <li><a href="/about">About</a></li>
+            <li><a href="/info">Info</a></li>
+            <li><a href="/contact">Contact</a></li>
+            </ul>
+        </nav>
+
+    </div>
+  )
+}
+
+export default Header
--- solarSystem/src/components/landingPage.tsx
+++ solarSystem/src/components/landingPage.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import './landingPage.css'
+import Layout from './Layout/Layout'
 
 const landingPage = () => {
   return (
-    <>
-    <div>landingPage</div>
-    </>
+    <Layout>
+      <div className="landingPage">
+        <h1>Welcome to the Solar System</h1>
+        <p>Learn about the planets in our solar system</p>
+      </div>
+    </Layout>
   )
 }
 
